963c0291abe779cbe7f90218829254638f04b2e5
Damsbot64 <damien64durand@gmail.com>
2023-02-08 23:41:29 +0100

    navbar ui done

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function NavBar() {
+  return (
+    <div className="bg-blueStrateg_in h-20 p-4 ">
+      <div className="flex  w-fit items-center h-full">
+        <p className="text-white text-3xl font-semibold">Strateg.in</p>
+      </div>
+    </div>
+  );
+}
+
+export default NavBar;
