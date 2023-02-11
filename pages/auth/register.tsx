d31888058812c48afe71635fbe5d4a31fd2d1c86
Damsbot64 <damien64durand@gmail.com>
2023-02-11 13:59:01 +0100

    input form and few fix

--- pages/auth/register.tsx
+++ pages/auth/register.tsx
@@ -1,7 +1,8 @@
 import { useAuth } from "@/context/UserContext";
-import axios from "axios";
 import React from "react";
 import { FieldValues, useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
 
 type TCredentials = {
   email: string;
@@ -9,8 +10,17 @@ type TCredentials = {
   firstName: string;
   lastName: string;
 };
+const schema = yup.object({
+  lastName: yup.string().required("Please provide a firstname"),
+  firstName: yup.string().required("Please provide a lastname"),
+  email: yup
+    .string()
+    .email("Please provide a valid mail")
+    .required("Please provide a email"),
+  password: yup.string().required("Please provide a password"),
+});
 
-function Register() {
+const Register = () => {
   const { register } = useAuth();
 
   const onSubmit = (data: FieldValues) => {
@@ -25,11 +35,13 @@ function Register() {
     register: userRegister,
     handleSubmit,
     formState: { errors },
-  } = useForm<TCredentials>();
+  } = useForm<TCredentials>({
+    resolver: yupResolver(schema),
+  });
 
   return (
-    <div className="flex justify-center mt-10">
-      <div className="flex flex-col w-[80%] rounded-md bg-white shadow-2xl pb-6">
+    <div className="flex justify-center items-center h-screen">
+      <div className="flex flex-col w-[80%]  md:w-[60%] lg:w-[40%] rounded-md bg-white shadow-2xl pb-6">
         <div className="p-2 w-fit h-12">
           <p>
             <span className="text-blueStrateg_in">Strateg</span>.in
@@ -37,34 +49,46 @@ function Register() {
         </div>
         <div className="flex justify-center ">
           <form className="w-[70%] flex flex-col items-center space-y-4">
-            <p className="text-blueStrateg_in">Register your Account</p>
-            <hr className="w-[50%] bg-blueStrateg_in h-1 rounded-full " />
+            <div className="flex flex-col items-center space-y-1">
+              <p className="text-blueStrateg_in">Register your Account</p>
+              <hr className="bg-blueStrateg_in h-1 rounded-full w-[75%]" />
+            </div>
             <input
               {...userRegister("firstName", { required: true })}
               type="text"
               placeholder="Firstname"
               className="bg-slate-100 rounded-md"
             />
+            {errors.firstName && (
+              <p className="text-red-600 text-xs">{errors.firstName.message}</p>
+            )}{" "}
             <input
               {...userRegister("lastName", { required: true })}
               type="text"
               placeholder="Lastname"
               className="bg-slate-100 rounded-md"
             />
+            {errors.lastName && (
+              <p className="text-red-600 text-xs">{errors.lastName.message}</p>
+            )}
             <input
               {...userRegister("email", { required: true })}
               type="email"
               placeholder="Email"
               className="bg-slate-100 rounded-md"
             />
-
+            {errors.email && (
+              <p className="text-red-600 text-xs">{errors.email.message}</p>
+            )}
             <input
               {...userRegister("password", { required: true })}
               type="password"
               placeholder="Password"
               className="bg-slate-100 rounded-md"
             />
-
+            {errors.password && (
+              <p className="text-red-600 text-xs">{errors.password.message}</p>
+            )}
             <button
               onClick={handleSubmit(onSubmit)}
               className="bg-blueStrateg_in text-white font-bold px-4 py-1 rounded-full"
@@ -76,6 +100,6 @@ function Register() {
       </div>
     </div>
   );
-}
+};
 
 export default Register;
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,10 +2,10 @@ import { useAuth } from "@/context/UserContext";
 import Image from "next/image";
 import React from "react";
 
-function NavBar() {
+const NavBar = () => {
   const { signOut } = useAuth();
   return (
-    <div className="bg-blueStrateg_in h-20 p-4 flex  ">
+    <div className="bg-blueStrateg_in h-20 p-4 flex">
       <div className="flex items-center justify-between w-full h-full">
         <p className="text-white text-3xl font-semibold">Strateg.in</p>
         <Image
@@ -18,6 +18,6 @@ function NavBar() {
       </div>
     </div>
   );
-}
+};
 
 export default NavBar;
